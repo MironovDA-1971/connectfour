dae2994da4425ff0fa2af2cb3c496427a2532cc8
MironovDA-1971 <mironovda@gmail.com>
2022-11-23 23:47:24 +0300

    Ready Stage 2/5: Game board

--- Main.kt
+++ Main.kt
@@ -3,4 +3,9 @@ package connectfour
 fun main() {
     println("Connect Four")
     val players = Players.list
+    val board = InitBoard.sizeBoard
+    println("${players[1]} VS ${players[2]}")
+    println("${board[0]} X ${board[1]} board")
+    BoxConstructor.drawBox(board)
+
 }
--- classes.kt
+++ classes.kt
@@ -18,21 +18,57 @@ object Players {
     )
 }
 
-object InputField {
-    val rowsColumns: List<String>
+object InitBoard {
+    val sizeBoard: List<Int>
         get() = checkInputField()
 
-    private fun checkInputField(): List<String> {
+    private fun checkInputField(): List<Int> {
         val range = 5..9
-        //val a: List<String>
+        var sizeBoardInt: List<Int>
+        val regex = """^(\d)+\s*x\s*(\d)+$""".toRegex()
+
         while (true) {
             println("Set the board dimensions (Rows x Columns)\n" +
                     "Press Enter for default (6 x 7)")
-            val a = readln().trim().split("\d\s?[xX]\s?\d")
-            if (a.size != 3 || a[1] != "x") println("Invalid input")
-            else if (a[0].toInt() !in range) println("Board rows should be from 5 to 9")
-            else if (a[0].toInt() !in range) println("Board columns should be from 5 to 9")
-            else break
+            val string = readln().trim().lowercase()
+            if (string.isNotEmpty()) {
+                try {
+                    val (a, b) = regex.find(string)!!.destructured
+                    sizeBoardInt = listOf(a.toInt(), b.toInt())
+                    if (sizeBoardInt[0] !in range) println("Board rows should be from 5 to 9")
+                    else if (sizeBoardInt[1] !in range) println("Board columns should be from 5 to 9")
+                    else break
+                } catch (_: Exception) {
+                    println("Invalid input")
+                }
+
+            } else {
+                sizeBoardInt = listOf(6, 7)
+                break
+            }
         }
+        return sizeBoardInt
     }
+}
+
+object BoxConstructor {
+            // ╚ ═ ╩ ╝ ║
+    var list: List<Int> = emptyList()
+
+    fun drawBox(list: List<Int>) {
+        var numberRow = ""
+        var oneRow = "║"
+        var bottomRow = "╚═"
+        for (num in 1..list[1]) {
+            numberRow += " $num"
+            oneRow += " ║"
+            bottomRow += if (num != list[1]) "╩═" else "╝"
+        }
+        for (num in 0..list[0]) {
+            if (num == 0) println(numberRow) else  println(oneRow)
+            if (num == list[0]) println(bottomRow)
+        }
+
+    }
+
 }
