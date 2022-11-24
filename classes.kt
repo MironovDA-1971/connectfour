3e0c2e605406cfa79a2d3da2f70ac995179875d0
MironovDA-1971 <mironovda@gmail.com>
2022-11-24 03:01:30 +0300

    Work on project. Stage 3/5: Game logic

--- classes.kt
+++ classes.kt
@@ -51,24 +51,90 @@ object InitBoard {
     }
 }
 
-object BoxConstructor {
-    //        // ╚ ═ ╩ ╝ ║
-    //var list: List<Int> = emptyList()
+class BoxConstructor(private val playerName: Map<Int, String>,
+                     private val boardSize: List<Int>
+) {
+    private var turnList: List<MutableList<Char>> = List(boardSize[0]){ MutableList(boardSize[1]){' '} }
 
-    fun drawBox(list: List<Int>) {
-        var numberRow = ""
-        var oneRow = "║"
+
+    private fun getTurn(playerNum: Int): Boolean {
+        val rangeColumn = 1..boardSize[1]
+        var turn: String
+        var stopGame = false
+        val playerIndex = (playerNum % 2) + 1
+        while (true) {
+            println("${playerName[playerIndex]}'s turn:")
+            try {
+                turn = readln().lowercase()
+                if(turn == "end") {
+                    stopGame = true
+                    break
+                }
+                if (turn.toInt() !in rangeColumn) println("The column number is out of range (1 - ${boardSize[1]})")
+                else break
+            } catch (_: Exception) {
+                println("Incorrect column number")
+            }
+        }
+        if (!stopGame) fillBox(turn.toInt() - 1, playerIndex)
+        return stopGame
+    }
+
+    private fun fillBox(turnNumber: Int, playerIndex: Int) {
+        for (i in turnList.lastIndex downTo 0) {
+            if (turnList[i][turnNumber] == ' ') {
+                if (playerIndex == 1) turnList[i][turnNumber] = 'o'
+                else turnList[i][turnNumber] = '*'
+                break
+            }
+        }
+    }
+
+    fun drawBox() {
+        var playerNumber = 1
+        var numberItm = ""
+        val oneItm = "║"
         var bottomRow = "╚═"
-        for (num in 1..list[1]) {
-            numberRow += " $num"
-            oneRow += " ║"
-            bottomRow += if (num != list[1]) "╩═" else "╝"
-        }
-        for (num in 0..list[0]) {
-            if (num == 0) println(numberRow) else  println(oneRow)
-            if (num == list[0]) println(bottomRow)
-        }
+        var flag = 0
+        while (true) {
+            if(getTurn(++playerNumber)) {
+                println("Game over!")
+                break
+            }
 
+            for(i in turnList.indices) {
+                for (num in turnList[i].indices) {
+                    if (i == 0) {
+                        numberItm += " ${num + 1}"
+                        if (flag == 0) bottomRow += if (num != turnList[i].lastIndex) "╩═" else "╝"
+                    }
+                    else {
+                        if (flag == 0) {
+                            println(numberItm)
+                            flag = 1
+                        }
+                        if (num != turnList[i].lastIndex) print("$oneItm${ turnList[i][num] }")
+                        else println(oneItm)
+                    }
+                }
+
+                if (i == turnList.lastIndex) println(bottomRow)
+            }
+        }
+        /*
+        var numberItm = ""
+        var oneItm = "║"
+        var bottomRow = "╚═"
+        for (num in 1..boardSize[1]) {
+            numberItm += " $num"
+            oneItm += " ║"
+            bottomRow += if (num != boardSize[1]) "╩═" else "╝"
+        }
+        for (num in 0..boardSize[0]) {
+            if (num == 0) println(numberItm) else  println(oneItm)
+            if (num == boardSize[0]) println(bottomRow)
+        }
+        */
     }
 
 }
